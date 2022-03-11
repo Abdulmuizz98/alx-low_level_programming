d4468aef37859270f70ad663f4786dbd4b1574a6
Abdulmuizz <hamzatabdulmuizz98@gmail.com>
2022-03-10 20:17:39 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Prints the assigned memory space for different data tyoes
+ * Return: return 0 after program execution
+ *
+ */
+int main(void)
+{	
+	char c;
+	int i;
+	long int a;
+	long long int b;
+	float d;
+
+	printf("ٍSize of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("ٍSize of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("ٍSize of a long int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("ٍSize of a long long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(d));
+	return (0);
+}
