a352e3b43f306de582d361f39cf120423bff6c3e
Abdulmuizz <hamzatabdulmuizz98@gmail.com>
2022-03-10 19:35:19 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Returns: zero
+ * Return: 0
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
