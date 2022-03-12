fa51f846ae38a77b2c52dff5d53899590b2910c7
Abdulmuizz <hamzatabdulmuizz98@gmail.com>
2022-03-12 14:30:08 -0800

    print 3 combos

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+/**
+ * main - print all the possible different comos of 3 digits..
+ * DESCRIPTION: use the putchar method to print all the
+ * possible different combinations of 3 digits.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	for (int i = 48; i <= 57; i)
+	{
+		for (int j = 49; j <= 57; )
+		{
+			for (int k = 50; k <=57;)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if( k == 57 && j == 56 && i == 55)
+				{
+					break;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				/*Increment Second from third */
+				if (k == 57)
+				{
+					j++;
+					k = j + 1;
+				}
+				else
+				{
+					k++;
+				}
+				/*Increment First from Second*/
+				if (j == 57)
+				{
+					i++;
+					j = i + 1;
+				}
+				else
+				{
+					j++;
+				}
+
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
