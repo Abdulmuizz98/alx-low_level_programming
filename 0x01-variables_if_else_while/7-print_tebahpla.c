1d804cbc9452bd0c1c40d7c3a14589443f84c269
Abdulmuizz <hamzatabdulmuizz98@gmail.com>
2022-03-12 10:38:47 -0800

    print alphabets backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all the letters of the english alphabet backwards 
+ * DESCRIPTION: use the putchar method to print all the
+ * letters of the english alphabets backwards.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char first = 'z';
+
+	while (first >= 'a')
+	{
+		putchar(first);
+		first--;
+	}
+	return (0);
+}
