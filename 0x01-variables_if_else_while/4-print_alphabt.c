f0117c82fda31abaa620dbfc6818874422443e83
Abdulmuizz <hamzatabdulmuizz98@gmail.com>
2022-03-12 09:09:50 -0800

    print alphabets except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print all the letters of the english alphabet except e and q.
+ * DESCRIPTION: use the putchar method to print all the
+ * letters of the english alphabets except e and q with one line afterwards.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char first = 'a';
+	while (first <= 'z')
+	{
+		if (first == 'e' || first == 'q') 
+			continue;
+		putchar(first);
+		first++;
+	}
+	putchar('\n');
+	return (0);
+}
