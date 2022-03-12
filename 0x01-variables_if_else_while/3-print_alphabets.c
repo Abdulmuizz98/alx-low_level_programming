8f5372162590279326156b1080b165794896a47d
Abdulmuizz <hamzatabdulmuizz98@gmail.com>
2022-03-12 08:32:33 -0800

    print both letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - print all the letters of the english alphabet.
+ * DESCRIPTION: use the putchar method to print all the
+ * letters of the english alphabets (small caps then uppercase)
+ * with one line afterwards.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char first = 'a';
+	char second = 'A';
+
+	while (first <= 'z')
+	{
+		putchar(first);
+		first++;
+	}
+	while (second <= 'Z')
+	{
+		putchar(second);
+		second++;
+	}	
+	putchar('\n');
+	return (0);
+}
