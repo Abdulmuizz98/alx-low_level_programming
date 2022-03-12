bf1c7c62c0bd560ec0bb13edea09c271fbc9b94e
Abdulmuizz <hamzatabdulmuizz98@gmail.com>
2022-03-12 06:16:37 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all the letters of the english alphabet.
+ * DESCRIPTION: use the putchar method to print all the
+ * letters of the english alphabet with one line afterwards.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char first = 'a';
+	while (first <= 'z')
+	{
+		putchar(first);
+		first++;
+	}
+	putchar("\n");
+	return (0);
+}
