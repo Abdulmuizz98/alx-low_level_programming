6e47346a65569c8625aeb1ebbe1a145ad6f169a7
Abdulmuizz <hamzatabdulmuizz98@gmail.com>
2022-03-12 10:22:03 -0800

    numbers with no char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print all the single digit numbers of base 10..
+ * DESCRIPTION: use the putchar method to print all the
+ * single digit numbers of base 10..
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int first = 0;
+
+	while (first <= 9)
+	{
+		putchar(first);
+		first++;
+	}
+	putchar('\n');
+	return (0);
+}
