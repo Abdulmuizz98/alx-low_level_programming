4bf82c42ff1a19cb899ea4c504792d527fd92b22
Abdulmuizz <hamzatabdulmuizz98@gmail.com>
2022-03-21 19:29:00 -0700

    array & pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates the value of n to 98
+ * @n: integer pointer to var int to be updated
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
