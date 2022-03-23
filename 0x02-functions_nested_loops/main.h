11f5e69094634271410736ed3341db4d68e30050
Hamzat Abdulmuizz O <75758703+Abdulmuizz98@users.noreply.github.com>
2022-03-23 23:41:28 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-nt _putchar(char c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -10,4 +10,4 @@ void jack_bauer(void);
 int add(int a, int b);
 void print_to_98(int n);
 void times_table(void);
-void print_times_table(int n);i
+void print_times_table(int n);
