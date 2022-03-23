5e46d2e384e2899ba02f02ec8c4cec518d4a166e
Hamzat Abdulmuizz O <75758703+Abdulmuizz98@users.noreply.github.com>
2022-03-23 23:52:37 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * * main - Print the word "Holberton"
- * *
- * * Return: Always 0 (Success)
- * */
-
+ * main - Print the word "Holberton"
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
