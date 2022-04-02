064c87eda611fc51e1f02680c16119d06be163d4
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-04-02 06:18:13 +0100

    find char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *_strchr - returns memory of the first occurence
+ * 		of a byte
+ * @s - string to be traversed
+ * @c - character to be found
+ *
+ * Return: Memory of first occurence if found, else NULL.
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *p;
+	
+	p = s;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+			break;
+
+		i++;
+		p++;
+	}
+	
+	if (*p)
+		return (p);
+	else 
+		return ('\0');
+}
