c32e0e974d2638ab6624d53267ae846b04249476
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-04-05 15:31:59 +0100

    get squareroot recursively

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-int check_prime_twice(int n);
+int check_prime_twice(int n, int i);
 
 /**
  * _sqrt_recursion - get square root of a number recursively
@@ -18,7 +18,7 @@ int _sqrt_recursion(int n)
 	if (n == 1)
 		return (1);
 
-	num = check_prime_twice(n);
+	num = check_prime_twice(n, 2);
 
 	if (num == 0)
 		return (-1);
@@ -30,19 +30,18 @@ int _sqrt_recursion(int n)
  * check_prime_twice - checks for a number that can divide n
  * twice without remainder.
  * @n: the number to be divided.
+ * @i: number to be divided by.
  *
  * Return: if found, the number that can divide it.
  * else, 0;
  */
-int check_prime_twice(int n)
+int check_prime_twice(int n, int i)
 {
-	int i = 2;
+	if (n <= i)
+		return (0);
 
-	while (n > i)
-	{
-		if (!(n % (i * i)))
-			return (i);
-		i++;
-	}
-	return (0);
+	if (!(n % (i * i)))
+		return (i);
+
+	return (check_prime_twice(n, (i + 1)));
 }
