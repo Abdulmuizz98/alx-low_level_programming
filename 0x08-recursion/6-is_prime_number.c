998f8a331bd2dcd5af3d3b0810a7d2355d4041a0
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-04-05 16:22:44 +0100

    check prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,10 +11,7 @@ int check_prime(int n, int i);
  */
 int is_prime_number(int n)
 {
-	if (n < 0)
-		n *= -1;
-
-	if (n == 0 || n == 1)
+	if (n == 0 || n == 1 || n < 0)
 		return (0);
 
 	return (check_prime(n, 2));
