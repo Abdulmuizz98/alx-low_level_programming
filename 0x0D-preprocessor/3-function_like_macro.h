d1ea08772518d793b9b9c046fca9c2e28fda2dba
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-04-13 00:58:48 +0100

    define ABS function-like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/**
+ * ABS - defines a function-like macro that
+ * computes the absolute value of a number
+ */
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif /*FUNCTION_LIKE_MACRO_H definition*/
