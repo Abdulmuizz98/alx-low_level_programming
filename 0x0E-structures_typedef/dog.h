085356cfad2c1b4397eef796b0555b9cfa7e4aea
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-04-13 10:54:09 +0100

    define dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - create a dog struct
+ * @name: reps the name of the dog
+ * @age: reps the age of the dog
+ * @owner: reps the owner of the dog
+ *
+ * Description: create a dog struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*DOG_H* definition/
