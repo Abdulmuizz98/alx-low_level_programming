2ccb71c5fd055fba6d393914024ae58ea91c4aa8
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-04-13 01:06:52 +0100

    define SUM function-like macro

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef SUM_H
+#define SUM_H
+/**
+ * SUM - defines a function-like macro that
+ * computes the sum of 2 numbers
+ * @x: first number to be added
+ * @y: second number to be added
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif /*SUM_H definition*/
