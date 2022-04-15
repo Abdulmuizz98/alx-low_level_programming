db78dd59b595b8dc91f8e380abe6c27b31ac4d76
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-04-15 23:55:04 +0100

    implement sum all variadic function

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * op_add - function that adds two integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: the result of the addition
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function that subtracts two integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: the result of the subtraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function that multiplies two integers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result of the multiplication
+ */
+int op_mul(int a, int b);
+{
+	return (a * b);
+}
+/**
+ * op_div - function that divides two integers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the result of the division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function that performs modulo operatio
+ * @a: the first number
+ * @b: the sevond number
+ *
+ * Return: the result of the modulo operation
+ */
+int op_mod(int a, int b)
+{	
+	return (a % b);
+}
---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - function that sums up all arguments.
+ * @n: size of the arguments.
+ * @...: variable number of arguments to be summed up.
+
+ * Return: if n is 0, 0;
+ * otherwise the sum of the arguments excluding the
+ * first (denoting the size).
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int sum = 0, i = 0;
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
+	va_end(list);
+	return (sum);
+}
