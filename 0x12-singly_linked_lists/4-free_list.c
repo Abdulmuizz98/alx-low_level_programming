31c232e8c12f717a56b336bf1731587f30434941
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-04-25 16:16:41 +0100

    implement (free_list) frees a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * free_list - frees a list.
+ * @head: the pointer to the head of the list
+ * 
+ * Return: Nothing.
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+	{
+		free(head);
+		return;
+	}
+	free_list(head->next);
+	free(head->str);
+	free(head);
+}
