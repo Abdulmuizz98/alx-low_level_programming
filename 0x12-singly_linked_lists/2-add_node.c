ae6810702758089835c06fe99b3daf1298c8e3c2
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-04-25 14:31:29 +0100

    implement (add_node) add elements to top of list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,15 +11,12 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	/*create the new node*/
 	list_t *newNode;
 	int i = 0;
-        
-	newNode	= malloc(sizeof(list_t));
-	
+
+	newNode = malloc(sizeof(list_t));
 	if (newNode == NULL)
 		return (NULL);
-	
 	newNode->str = strdup(str);
 	/*find string length*/
 	while (str[i] != '\0')
