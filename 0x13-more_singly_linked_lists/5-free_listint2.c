67b7fd3527f0e116c24715a44aac1161aedfe677
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-04-27 01:18:58 +0100

    free list 2n type 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * free_listint2 - frees an integer list recursively
+ * and sets head to NULL.
+ * @head: the pointer to the head of the list
+ *
+ * Return: Nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+	
+	while (*head)
+	{
+		current = (*head)->next;
+		free(*head);
+		*head = current;
+	}
+}
