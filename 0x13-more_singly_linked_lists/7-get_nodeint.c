367d25858ff440e915b09d0603d5f9eea2c4bfa9
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-04-27 02:09:24 +0100

    implement (get_nodeint) get the node at an index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * get_nodeint_at_index - function that gets the nth node of a linked list
+ * @head: the pointer to the first node of the list.
+ * @index: the index of the integer to be fetched.
+ *
+ * Return: the integer at the nth position;
+ * Otherwise if it doesn't exist NULL.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	
+	while (i <= index && head)
+	{
+		head = head->next;
+		i++;
+	}
+	return (head);
+}
