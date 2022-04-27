1dad0303a18b3089fdf2610ab5678744f425e8af
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-04-27 01:24:53 +0100

    free list 2n type 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,14 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current;
-
+	
+	if (head == NULL)
+		return;	
 	while (*head)
 	{
 		current = (*head)->next;
 		free(*head);
 		*head = current;
 	}
+	head == NULL;
 }
