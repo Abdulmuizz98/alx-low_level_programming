c229cae664939c75f681cf77e010a06671bb65a0
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-04-27 02:20:02 +0100

    implement (get_nodeint) get the node at an index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,12 +12,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 	
-	if (index < 0)
-		return (NULL);
 	while (i <= index && head)
 	{
 		head = head->next;
 		i++;
 	}
-	return (head);
+	if (head == NULL)
+		return (NULL);
+	else
+		return (head);
 }
