f7039e0abbc9db53007d07d85aca8b3f633931af
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-05-01 16:39:20 +0100

    implement (get_bit) gets the bit at a particular index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,59 +12,9 @@ node *add_node(node **head, const int n);
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i = 0;
-	int rem = 0;
-	node *head = NULL;
-
-	if (n == 0)
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	if ((n & (1 << index)) == 0)
 		return (0);
-	while (n != 0)
-	{
-		rem = n % 2;
-		n = n / 2;
-		add_node(&head, rem);
-	}
-
-	while (i < index)
-	{
-		if (head == NULL)
-			return (-1);
-		i++;
-		head = head->next;
-	}
-	return (head->n);
-}
-/**
- * add_node - add a node to the end of the list.
- * @head: the pointer to the head of the list
- * @n: the integer to be added to the list
- *
- * Return: the address of the new element;
- * if it fails, NULL.
- */
-node *add_node(node **head, const int n)
-{
-	node *newNode;
-	node *trav;
-
-	newNode = malloc(sizeof(node));
-	if (newNode == NULL)
-		return (NULL);
-	newNode->n = n;
-	/*search for the last node and point its next to the new node*/
-	if (*head == NULL) /*if the list is empty*/
-	{
-		*head = newNode;
-	}
-	else /*if list in not empty*/
-	{
-		trav = *head;
-		while (trav->next)
-			trav = trav->next;
-
-		trav->next = newNode;
-	}
-	newNode->next = NULL;
-	/*return the new node*/
-	return (newNode);
+	return (1);
 }
