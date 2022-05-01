c2d2501ce8776e69876aec578b55e320f70d81ad
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-05-01 16:23:03 +0100

    implement (get_bit) gets the bit at a particular index

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 int modulo(unsigned long int num);
-void print_bin(unsigned long int num);
 /**
  * print_binary - print the binary representation of a number
  * @n: the integer to be converted to binary
@@ -12,7 +11,7 @@ void print_bin(unsigned long int num);
 void print_binary(unsigned long int n)
 {
 	unsigned long int rem;
-	
+
 	if (n > 1)
 		print_binary(n >> 1);
 
