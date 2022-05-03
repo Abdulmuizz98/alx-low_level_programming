e432b5ae7a01443b10b9eba6ffeac2b764dc8d94
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-05-03 22:12:12 +0100

    implement (abs) cocompute absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of a number
+ * @num: the number to compute its absolute
+ *
+ * Return: the absolute value of the number
+ */
+int _abs(int num)
+{
+	return ((num < 0) ? -num : num);
+}
