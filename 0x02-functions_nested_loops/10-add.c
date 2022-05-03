74867c29880b6d142c3d3e97b4c80fac07d07a38
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-05-04 00:10:45 +0100

    implement (add) sum two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @num1: the first sum operand
+ * @num2: the second sum operand
+ *
+ * Return: the result of the sum of the two integers
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
