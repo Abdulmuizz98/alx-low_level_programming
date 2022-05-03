d27b559aa7f6b5031b4949d10f678d60fe426115
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-05-03 20:16:42 +0100

    update print combination 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,33 +1,43 @@
 #include <stdio.h>
 /**
- * main - print all the possible different comos of 3 digits..
+ * main - print all the possible different combos of a set of 2 digits..
  * DESCRIPTION: use the putchar method to print all the
- * possible different combinations of 3 digits.
+ * possible different combinations of a set of 2 digits.
  *
  * Return: 0
  */
 int main(void)
 {
-	int i, j, k;
+	int i, j, k, l;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 0, j = 0; i < 10; j++)
 	{
-		for (j = i + 1; j <= 57; j++)
+		if (j == 10)
 		{
-			for (k = j + 1; k <= 57; k++)
+			j = 0;
+			i++;
+			if (i == 10)
+				continue;
+		}
+
+		for (k = i, l = j + 1; k < 10; l++)
+		{
+			if (l == 10)
 			{
-				putchar(i);
-				putchar(j);
-				putchar(k);
-				if (k == 57 && j == 56 && i == 55)
-				{
-					break;
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				l = 0;
+				k++;
+				if (k == 10)
+					continue;
+			}
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(' ');
+			putchar(k + '0');
+			putchar(l + '0');
+			if (!(i == 9 && j == 8 && k == 9 && l == 9))
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
