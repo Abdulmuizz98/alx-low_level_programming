5f7390a41047aa2d618118e638c726d5623399b8
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-05-04 12:04:19 +0100

    sum of natural number multiplesof 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+unsigned int print_multiples_3_5(int num);
+/**
+ * main - prints the sum of multiples of 3 and 5 before 1024
+ * 
+ * Return: Nothing.
+ */
+int main(void)
+{
+	int num;
+
+	num = print_multiples_3_5(1024);
+	printf("%u", num);
+
+	return (0);
+}
+/**
+ * print_multiples_3_5 - print the sum of multiples
+ * @num: the number to print its multiples
+ *
+ * Return: the last digit
+ */
+unsigned int print_multiples_3_5(int num)
+{
+	int i = 0;
+	int sum = 0;
+
+	while (i < num)
+	{
+		if ((num % 3) == 0 || (num % 5) == 0)
+			sum += num;
+		num++;
+	}
+	return (sum);
+}
