989d86823059e022a997db4c8b890efe367b611d
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-05-12 02:18:26 +0100

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int count = 0;
 
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1 || filename == NULL)
 	{
 		exit(1);
