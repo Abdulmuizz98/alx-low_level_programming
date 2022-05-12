84c485f1c00809f3d6383935b17075a689289671
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-05-12 03:45:03 +0100

    create a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/**
+ *
+ *
+ *
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, count = 0, write_bytes;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+		while (text_content[count])
+			count++;
+	fd = open(filename, O_WRONLY);
+	if (fd == -1)
+		return (-1);
+	write_bytes = write(fd, text_content, count);
+	if (write_bytes == -1)
+		return (-1);
+	close(fd);
+	return (1);
+
+}
