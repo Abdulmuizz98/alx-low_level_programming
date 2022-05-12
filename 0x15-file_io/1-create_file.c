9f44e6cbd0886a63bea6a22eb2ceaecc58a52c8c
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-05-12 02:14:06 +0100

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+/**
+ * create_file - creates a file and adds initial text
+ * @filename: the name of the file to create
+ * @text_content: the initial text to be added.
+ *
+ * Return: -1 on failure, 1 on success
+ * -1 if filename is null.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int count = 0;
+
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (fd == -1 || filename == NULL)
+	{
+		exit(1);
+		return (-1);
+	}
+
+	while (text_content[count])
+		count++;
+	if (count != 0)
+		write(fd, text_content, count);
+	close(fd);
+	return (1);
+}
