427e27babf319b2df597d7b1325477c47a0639ea
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-06-10 05:38:17 +0100

    implement (get_dnodeint_at_index) to get a node at an index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * get_dnodeint_at_index - gets node at an indexfrees a l
+ * @head: the pointer to the head of the list
+ * @index: index of the node to be gotten
+ * 
+ * Return: the node at that index;
+ * otherwise, if it does not exit NULL.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *trav;
+	unsigned int i = 0;
+
+	trav = head;
+	while (trav && i <= index)
+	{
+		if (i == index)
+			return (trav);
+		trav = trav->next;
+		i++;
+	}
+	return (NULL);
+}
+
