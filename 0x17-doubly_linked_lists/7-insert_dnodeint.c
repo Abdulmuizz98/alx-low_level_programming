b663dcba8cf8d143f566ee560264cf52379aacb3
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-06-10 18:10:46 +0100

    implement (insert_dnodeint) to insert at an index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,8 +13,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *trav, *new_node, *temp;
 	unsigned int i = 0;
- 
-	/* traverse to the index */	
+
+	/* traverse to the index */
 	trav = *h;
 	while (trav && i < idx)
 	{
@@ -26,22 +26,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (i == 0) /* Insert at the beginning */
 		return (add_dnodeint(h, n));
-	else /* Insert anywhere else */
+	/* Insert anywhere else */
+	/*create the new node*/
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 	{
-		/*create the new node*/
-		new_node = malloc(sizeof(dlistint_t));
-		if (new_node == NULL)
-		{
-			dprintf(2, "Error: Can't malloc\n");
-			return (NULL);
-		}
-		new_node->n = n;
-		new_node->next = trav;
-		new_node->prev = trav->prev;
-		temp = trav;
-		(trav->prev)->next = new_node;
-		temp->prev = new_node;
+		dprintf(2, "Error: Can't malloc\n");
+		return (NULL);
 	}
+	new_node->n = n;
+	new_node->next = trav;
+	new_node->prev = trav->prev;
+	temp = trav;
+	(trav->prev)->next = new_node;
+	temp->prev = new_node;
+
 	return (new_node);
 }
 
