db16f116cacb43919874055e4776b56d09f13dd0
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-06-11 01:07:04 +0100

    implement (insert_dnodeint) to insert at an index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,8 +14,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *trav, *new_node, *temp;
 	unsigned int i = 0;
 
-	if (h == NULL)
-		return (NULL);
+	/*if (h == NULL)
+		return (NULL);*/
 
 	if (idx == 0) /* Insert at the beginning */
 		return (add_dnodeint(h, n));
