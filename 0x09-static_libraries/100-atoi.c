a27baca430c681fcb794469f6e8431780f424657
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2022-11-01 20:39:41 +0100

    static lib 1

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,4 +1,4 @@
-#include "mainn.h"
+#include "main.h"
 
 /**
  *  * _atoi - convert a string to an integer.
@@ -14,6 +14,7 @@ int _atoi(char *s)
 
 	do {
 		if (*s == '-')
+			;
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
 		else if (num > 0)
