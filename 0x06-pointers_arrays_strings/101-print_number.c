6f72442a0a0009f390eef222b07d297d205d06c5
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-06-23 02:23:53 +0100

    feat: perfom magic to change array content

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * get_place - get the highest place value of a number.
+ * @n: The number to get its place value.
+ *
+ * Return: The place value.
+ */
+int get_place(int n)
+{
+	int place = 1;
+
+	while (n > 9)
+	{
+		place *= 10;
+		n /= 10;
+}
+
+	return (place);
+}
+
+/**
+ * print_number - prints a number
+ * @n: The number to print.
+ *
+ * Return: Nothing.
+ */
+void print_number(int n)
+{
+	int place;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	place = get_place(n);
+
+	while (n != 0)
+	{
+		_putchar((n / place) + 48);
+		n %= place;
+		place /= 10;
+	}
+
+}
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,7 +3,7 @@
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
- * 
+ *
  * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
@@ -15,22 +15,16 @@ char *cap_string(char *str)
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
-		if (str[index - 1] == ' ' ||
-		  str[index - 1] == '\t' ||
-		  str[index - 1] == '\n' ||
-		  str[index - 1] == ',' ||
-		  str[index - 1] == ';' ||
-		  str[index - 1] == '.' ||
-		  str[index - 1] == '!' ||
-		  str[index - 1] == '?' ||
-		  str[index - 1] == '"' ||
-		  str[index - 1] == '(' ||
-		  str[index - 1] == ')' ||
-		  str[index - 1] == '{' ||
-		  str[index - 1] == '}' ||
-		  index == 0)
-		  	str[index] -= 32;
-			
+		if (str[index - 1] == ' ' || str[index - 1] == '\t' ||
+			str[index - 1] == '\n' || str[index - 1] == ',' ||
+			str[index - 1] == ';' || str[index - 1] == '.' ||
+			str[index - 1] == '!' || str[index - 1] == '?' ||
+			str[index - 1] == '"' || str[index - 1] == '(' ||
+			str[index - 1] == ')' || str[index - 1] == '{' ||
+			str[index - 1] == '}' || index == 0
+			)
+			str[index] -= 32;
+
 		index++;
 	}
 	return (str);
