ef7092262672555fe709e8b2bbb44ec674da6769
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-06-28 01:19:00 +0100

    fix: _strstr - return pointer to haystack when needle is an empty string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * * _strcmp - Compares pointers to two strings.
+ * @s1: A pointer to the first string to be compared.
+ * @s2: A pointer to the second string to be compared.
+ *
+ * Return: If str1 < str2, the negative difference of
+ * the first unmatched characters.
+ * If s1 == s2, 0.
+ * If s1 > s2, the positive difference of
+ * the first unmatched characters.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 && *s2) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
+
+/**
  * *_strstr - Locate a substring..
  * @haystack: string checked for matching substring.
  * @needle: substring that is found.
@@ -11,6 +33,9 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i;
+	
+	if (!_strcmp(needle, ""))
+		return (haystack);
 
 	while (*haystack)
 	{
