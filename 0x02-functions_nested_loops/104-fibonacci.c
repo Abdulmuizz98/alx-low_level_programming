5a7edfc5c372ff021d43958e99bf2939e1646412
Abdulmuizz98 <hamzatabdulmuizz98@gmail.com>
2023-11-17 03:08:48 +0100

    fix: spaces at start of lines

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,21 +8,21 @@
 int main(void)
 {
 	double n1 = 1;
-  double n2 = 2;
-  double sum;
-  int i = 3;
-  
-  printf("1, 2,");
-  
+	double n2 = 2;
+	double sum;
+	int i = 3;
+
+	printf("1, 2,");
+
 	while (i < 98)
 	{
 		sum = n1 + n2;
-    n1 = n2;
-    n2 = sum;
+		n1 = n2;
+		n2 = sum;
 		printf(" %0.f,", sum);
 
 		i++;
 	}
-  printf(" %0.f\n", n1 + n2);
+	printf(" %0.f\n", n1 + n2);
 	return (0);
 }
