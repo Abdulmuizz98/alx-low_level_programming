dc0e9e192bdda1515e90830905511eb72de13369
Abdulmuizz98 <hamzatabdulmuizz98@gmail.com>
2023-11-17 02:55:51 +0100

    feat: implement function that prints first 98 fib numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-void print_number(unsigned long int num);
-unsigned long int fibonacci(int place);
 /**
  * main - prints the fib terms to 98
  *
@@ -9,67 +7,22 @@ unsigned long int fibonacci(int place);
  */
 int main(void)
 {
-	int i = 0;
-
+	double n1 = 1;
+  double n2 = 2;
+  double sum;
+  int i = 3;
+  
+  printf("1, 2,");
+  
 	while (i < 98)
 	{
-		print_number(fibonacci(1));
-		printf(", ");
+		sum = n1 + n2;
+    n1 = n2;
+    n2 = sum;
+		printf(" %0.f,", sum);
 
 		i++;
 	}
-	/*print_number(fibonacci(98));
-	printf("\n");
-	print_number(fibonacci(50));
-	printf("\n");*/
+  printf(" %0.f\n", n1 + n2);
 	return (0);
-}
-/**
- * fibonacci - print the place of fibonacci
- * @place: the postion of fib
- *
- * Return: the fibonacci value
- */
-unsigned long int fibonacci(int place)
-{
-	int i = 3;
-	unsigned long int num1 = 1;
-	unsigned long int num2 = 2;
-	unsigned long int sum;
-
-	if (place == 1)
-		return (num1);
-	if (place == 2)
-		return (num2);
-
-	while (i <= place)
-	{
-		sum = num1 + num2;
-		num1 = num2;
-		num2 = sum;
-		i++;
-	}
-	return (sum);
-}
-/**
- * print_number - prints positive integers with putchar
- * @num: the number to print
- */
-void print_number(unsigned long int num)
-{
-	unsigned long int place = 1;
-	unsigned long int temp = num;
-
-	while (temp / 10)
-	{
-		place *= 10;
-		temp /= 10;
-	}
-	while (place != 1)
-	{
-		_putchar((num / place) + '0');
-		num %= place;
-		place /= 10;
-	}
-	_putchar((num % 10) + '0');
 }
