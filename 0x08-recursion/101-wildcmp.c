2b2d58c14103e95fa9823a0c42d4c006b8b66963
Abdulmuizz98 <hamzatabdulmuizz98@gmail.com>
2023-11-18 23:45:58 +0100

    feat: implement wildcmp - match string withe wildcard

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,11 +1,12 @@
 #include "main.h"
 int sgmnt_len(char *s);
-int wild(char *s1, char *s2);
+int wild(char *s1, char *s2, char *pr);
 char *_strstr(char *s, char *d, int i, int j);
 int _is_substring(char *s, char *d, int i, int j);
+int _is_endstr(char *s, char *d, int i, int j);
 
 /**
- * wildcmp - Checks if a wildcard string matches
+ * wildcmp - wrapper for wild
  * identical string.
  * @s1: the string to match
  * @s2: the wildcard string.
@@ -15,21 +16,41 @@ int _is_substring(char *s, char *d, int i, int j);
  */
 int wildcmp(char *s1, char *s2)
 {
+	return (wild(s1, s2, 0));
+}
+
+/**
+ * wild - Checks if a wildcard string matches
+ * identical string.
+ * @s1: the string to match
+ * @s2: the wildcard string.
+ * @pr: the previous non-astericks segment.
+ *
+ * Return: if it matches 1;
+ * else, 0;
+ */
+int wild(char *s1, char *s2, char *pr)
+{
 	int len;
 	char *start;
 
 	if (*s2 == 0)
-		return ((*s1 == 0 || *(s2 - 1) == '*') ? 1 : 0);
+	{
+		if (*s1 == 0 || *(s2 - 1) == '*')
+			return (1);
+		len = sgmnt_len(pr);
+		return (_is_endstr(s1, pr, 0, len - 1) ? 1 : 0);
+	}
 	if (*s2 != '*')
 	{
 		len = sgmnt_len(s2);
 		start = _strstr(s1, s2, 0, len - 1);
 
-		return ((start == 0) ? 0 : wildcmp((start + len), (s2 + len)));
+		return ((start == 0) ? 0 : wild((start + len), (s2 + len), s2));
 	}
 	else
 	{
-		return (wildcmp(s1, s2 + 1));
+		return (wild(s1, s2 + 1, 0));
 	}
 }
 
@@ -88,4 +109,26 @@ int _is_substring(char *s, char *d, int i, int j)
 	if (*(s + i) != *(d + i) || *(s + j) != *(d + j))
 		return (0);
 	return (_is_substring(s, d, i + i, j - 1));
+}
+
+/**
+ * _is_endstr - checks if a string ends with
+ * a particular substring.
+ * @s: the haystack string
+ * @d: the needle string.
+ * @i: start delim of needle string.
+ * @j: end delim of needle string.
+ *
+ * Return: if is endstring, 1;
+ * else, 0;
+ */
+int _is_endstr(char *s, char *d, int i, int j)
+{
+	int res;
+	int len = j + 1;
+
+	if (*(s + len) != '\0')
+		return (_is_endstr(s + 1, d, i, j));
+	res = _is_substring(s, d, i, j);
+	return (res);
 }
